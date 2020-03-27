2f1d866051ff2fffca8fcbc5081975ce1bbec471
Jongmun-Park <momenttraveler@gmail.com>
2020-03-27 17:07:37 +0900

    test complete

--- artsy_server/art/models.py
+++ artsy_server/art/models.py
@@ -10,7 +10,8 @@ class Category(models.Model):
 
 class Art(models.Model):
     name = models.CharField(max_length=128)
-    category = models.ForeignKey(Category, related_name='arts', null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, related_name='arts', null=True,
+            on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
--- artsy_server/art/schema.py
+++ artsy_server/art/schema.py
@@ -15,11 +15,41 @@ class ArtType(DjangoObjectType):
         model = Art
 
 class Query(object):
+    category = graphene.Field(CategoryType,
+                            id=graphene.Int(),
+                            name=graphene.String())
     all_categories = graphene.List(CategoryType)
-    all_arts = graphene.List(Art)
+    art = graphene.Field(ArtType,
+                        id=graphene.Int(),
+                        name=graphene.String())
+    all_arts = graphene.List(ArtType)
+
+    def resolve_category(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Category.objects.get(id=id)
+
+        if name is not None:
+            return Category.objects.get(name=name)
+
+        return None
 
     def resolve_all_categories(self, info, **kwargs):
         return Category.objects.all()
+
+    def resolve_art(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Art.objects.get(id=id)
+
+        if name is not None:
+            return Art.objects.get(name=name)
+
+        return None
 
     def resolve_all_arts(self, info, **kwargs):
         return Art.objects.select_related('category').all()
