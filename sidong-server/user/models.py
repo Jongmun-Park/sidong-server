66ddf9bced782ba2919975ddc0571c45a754ed5e
Jongmun-Park <momenttraveler@gmail.com>
2020-12-01 17:53:50 +0900

    add Artist, Category and File model

--- sidong-server/user/models.py
+++ sidong-server/user/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 from phonenumber_field.modelfields import PhoneNumberField
+from file.models import File
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
 
 
 class Artist(models.Model):
@@ -9,5 +15,5 @@ class Artist(models.Model):
     real_name = models.CharField(max_length=32)
     phone = PhoneNumberField(null=True)
     description = models.TextField(blank=True)
-    # thumbnail =
-    # categories =
+    thumbnail = models.ForeignKey(File, null=True, on_delete=models.SET_NULL)
+    categories = ArrayField(models.CharField(max_length=64))
