d0578e485a3a2264cae8ab48e39f18c886c2a839
Jongmun-Park <momenttraveler@gmail.com>
2021-05-12 15:14:37 +0900

    add EMAIL settings

--- sidong-server/sidong_server/settings.py
+++ sidong-server/sidong_server/settings.py
@@ -39,6 +39,12 @@ ALLOWED_HOSTS = ["api.jakupteo.com", "localhost"]
 ADMINS = [('박종문', 'jakupteo@gmail.com')]
 SERVER_EMAIL = 'system@jakupteo.com'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
