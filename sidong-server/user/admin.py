33a8c6fd68bab41b829bfa9615954007394aefb8
Jongmun-Park <momenttraveler@gmail.com>
2021-06-03 11:38:11 +0900

    admin 모델 추가

--- sidong-server/user/admin.py
+++ sidong-server/user/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from user.models import Artist
+from user.models import Artist, UserInfo, Order, Payment, Refund
 
 
 class ArtistAdmin(admin.ModelAdmin):
     pass
 
 
+class UserInfoAdmin(admin.ModelAdmin):
+    pass
+
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    pass
+
+
+class RefundAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Artist, ArtistAdmin)
+admin.site.register(UserInfo, UserInfoAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Refund, RefundAdmin)
