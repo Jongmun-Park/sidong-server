4a293dc785747c4a3a154dd67eb2ddbc18d8fb72
Jongmun-Park <momenttraveler@gmail.com>
2021-06-22 18:23:11 +0900

    add search_result_count query

--- sidong-server/sidong_server/schema.py
+++ sidong-server/sidong_server/schema.py
@@ -1,12 +1,38 @@
-from graphene import ObjectType, Schema
+from graphene import ObjectType, Schema, Int, Field, Boolean, String
 import graphql_jwt
-
 import art.schema
 import file.schema
 import user.schema
+from django.db.models import Q
+from user.models import Artist
+from art.models import Art
 
 
-class Query(art.schema.Query,
+class SearchResultCountConnection(ObjectType):
+    result = Boolean()
+    art_count = Int()
+    artist_count = Int()
+
+
+class SearchQuery(ObjectType):
+    search_result_count = Field(
+        SearchResultCountConnection, word=String(required=True))
+
+    def resolve_search_result_count(self, info, word):
+        print('word:', word)
+        if not word:
+            return {'result': False}
+
+        return {
+            'result': True,
+            'art_count': Art.objects.filter(name__icontains=word).count(),
+            'artist_count': Artist.objects.filter(
+                Q(real_name__icontains=word) | Q(artist_name__icontains=word)).count(),
+        }
+
+
+class Query(SearchQuery,
+            art.schema.Query,
             file.schema.Query,
             user.schema.Query, ObjectType):
     # This class will inherit from multiple Queries
